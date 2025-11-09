c8319d0d68a01adcec2f7f2ea476dc3f352f9814
Dan Arhipov <chifmatsuno17@gmail.com>
2025-11-09 19:19:13 +0300

    Add tabs js & change primary colors a little

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './styles/styles.scss'
-
-import ThemeChange from './modules/ThemeChange'
+import ThemeChange from './modules/ThemeChange.js'
+import TabsCollection from '@/modules/TabsCollection.js'
 
 new ThemeChange()
-
+new TabsCollection()
